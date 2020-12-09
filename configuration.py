88ae9a800c007a27665c6ea68c7cda04cef07df6
Juan Pablo Becona <jpbecona@gmail.com>
2020-12-08 21:25:16 -0300

    Add enter_pin method

--- configuration.py
+++ configuration.py
@@ -4,8 +4,38 @@ import json
 from send_cmd import send_cmd
 from constants import MY_PHONE_PASS
 
-def start_up_nbiot(ser):
-    response_history=[]
+
+def enter_pin(ser, response_history=[]):
+    cmd_response = send_cmd("AT+CPIN?", ser, 6,  print_response=True)
+    response_history.append(cmd_response)
+
+    response_code = response_history[-1]["response"][1][0:-2] # [0:-2] to remove \r\n
+    
+    if response_code == "+CPIN: SIM PIN":
+        cmd_response = send_cmd("AT+CPIN=" + MY_PHONE_PASS, ser, 8,  print_response=True, ms_of_delay_after=1000)
+        response_history.append(cmd_response)
+        response_status = response_history[-1]["status"]
+
+        if response_status == "ERROR":
+            status = "ERROR"
+            message = "SIM PIN SENT AND ERROR"
+        elif response_status == "OK":
+            status = "OK"
+            message = "SIM PIN SENT AND SUCCESS"
+        else:
+            status = "ERROR"
+            message = "SIM PIN SENT AND UNFINISHED"
+    elif response_code == "+CPIN: READY":
+        status = "OK"
+        message = "+CPIN: READY"
+    else:
+        print('==> SIM error with message ' + response_code)
+        status = "ERROR"
+        message = response_code
+        
+    return({"response_history": response_history, "status": status, "message": message})
+
+def start_up_nbiot(ser, response_history=[]):
 
     cmd_response = send_cmd("AT", ser,  print_response=True, ms_of_delay_after=100)
     response_history.append(cmd_response)
@@ -22,8 +52,7 @@ def start_up_nbiot(ser):
     #TODO: log response_history
     #TODO: check if retries are needed
 
-def optimized_start_up_nbiot(ser):
-    response_history=[]
+def optimized_start_up_nbiot(ser, response_history=[]):
 
     res = enter_pin(ser, response_history)
     if res["status"] == "ERROR":
@@ -81,33 +110,7 @@ def check_nbiot_conection(ser, retries=3, response_history=[], custom_delay=2000
     print('\r\n' + status + ' - ' + message + '\r\n')
     return({"response_history": response_history, "status": status, "message": message})
 
-def enter_pin(ser, response_history=[]):
-    cmd_response = send_cmd("AT+CPIN?", ser, 6,  print_response=True)
-    response_history.append(cmd_response)
-
-    response_code = response_history[-1]["response"][1][0:-2] # [0:-2] to remove \r\n
-    
-    if response_code == "+CPIN: SIM PIN":
-        cmd_response = send_cmd("AT+CPIN=" + MY_PHONE_PASS, ser, 8,  print_response=True, ms_of_delay_after=1000)
-        response_history.append(cmd_response)
-        response_status = response_history[-1]["status"]
-
-        if response_status == "ERROR":
-            status = "ERROR"
-            message = "SIM PIN SENT AND ERROR"
-        elif response_status == "OK":
-            status = "OK"
-            message = "SIM PIN SENT AND SUCCESS"
-        else:
-            status = "ERROR"
-            message = "SIM PIN SENT AND UNFINISHED"
-    elif response_code == "+CPIN: READY":
-        status = "OK"
-        message = "+CPIN: READY"
-    else:
-        print('==> SIM error with message ' + response_code)
-        status = "ERROR"
-        message = response_code
-        
+def turn_off(ser, response_history=[]):
+    cmd_response = send_cmd("AT+QPOWD", ser,  print_response=True, ms_of_delay_after=1000)
+    response_history.append(cmd_response)        
     return({"response_history": response_history, "status": status, "message": message})
-
